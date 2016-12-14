069eb765a622648df3edf7f05ec80a6afc149a3a
Nassia <nassia@inuits.eu>
2016-12-14 15:01:29 +0100

    Updated schem and solrconfig

--- reindex-solr.sh
+++ reindex-solr.sh
@@ -2,4 +2,4 @@
 rake jetty:stop
 rm -r ./jetty/solr/blacklight-core/data/*
 rake jetty:start
-rake solr:marc:index MARC_FILE=./jetty/solr/blacklight-core/sample_marc_data.xml
+#rake solr:marc:index MARC_FILE=./jetty/solr/blacklight-core/sample_marc_data.xml
--- start-app.sh
+++ start-app.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+rake jetty:start
 rails server &
