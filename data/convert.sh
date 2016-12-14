ae0f8882cfe87e42ca959ef640596729b5171b79
Nassia <nassia@inuits.eu>
2016-12-14 15:00:45 +0100

    Example data stuff

--- data/convert.sh
+++ data/convert.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for file in ./3*.xml
+for file in ../msk-lido-data/*.xml
 do
-  catmandu convert LIDO to JSON --fix lido-to-solr-msk.fix --array 0 < "$file" > out.json
-  curl -X POST -H 'Content-Type: application/json' 'http://datahub_ubuntu:8983/solr/blacklight-core/update/json/docs' --data-binary @out.json
+    echo "$file"
+    iconv -f ISO-8859-1 -t UTF-8 "$file" > out.xml
+    #catmandu convert LIDO to JSON --fix lido-to-solr-msk.fix --array 0 < out.xml > out.json
+    #curl -X POST -H 'Content-Type: application/json' 'http://datahub_ubuntu:8983/solr/blacklight-core/update/json/docs?commit=true' --data-binary @out.json
 done
