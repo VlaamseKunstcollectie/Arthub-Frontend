3c42c94020f80969c892e03d6698f270b3dd0234
Matthias Vandermaesen <matthias@colada.be>
2017-10-20 16:41:17 +0200

    added: support for Lido XML view.

---
+++ app/models/concerns/blacklight/document/lido.rb
@@ -0,0 +1,21 @@
+# This module provide Dublin Core export based on the document's semantic values
+module Blacklight::Document::Lido
+  
+  def self.extended(document)
+    # Register our exportable formats
+    Blacklight::Document::Lido.register_export_formats( document )
+  end
+
+  def self.register_export_formats(document)
+    document.will_export_as(:xml, "text/xml")
+    document.will_export_as(:lido_xml, "application/lido+xml")
+  end
+
+  def export_as_oai_lido_xml
+    self.fetch(:xml)
+  end
+
+  alias_method :export_as_xml, :export_as_oai_lido_xml
+  alias_method :export_as_lido_xml, :export_as_oai_lido_xml
+
+end
