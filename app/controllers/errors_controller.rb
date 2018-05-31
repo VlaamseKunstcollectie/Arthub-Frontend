ca8cb6cd6df215188a6ae03d6283516176e1ff54
Matthias Vandermaesen <matthias@colada.be>
2018-05-31 10:31:59 +0200

    Fixed: missing template errors/* console error when in production mode.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,13 @@
 class ErrorsController < ApplicationController
   def not_found
-	render(:status => 404)
+	render(:status => 404, template: "errors/not_found")
   end
 
   def unprocessable_entity
-    render(:status => 422)
+    render(:status => 422, template: "errors/unprocessable_entity")
   end
 
   def internal_server_error
-    render(:status => 500)
+    render(:status => 500, template: "errors/internal_server_error")
   end
 end
