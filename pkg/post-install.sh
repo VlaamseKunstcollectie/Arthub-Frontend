eba93cab364accd5bd4362cab3c14802fdb5d472
jonas.de.moor <jonas.de.moor@inuits.eu>
2018-06-24 17:10:19 +0200

    pkg/post-install.sh: Do not use full path to RVM

--- pkg/post-install.sh
+++ pkg/post-install.sh
@@ -6,14 +6,23 @@ set -o pipefail
 
 chmod +x /opt/project-blacklight/start.sh
 
-echo "Reloading systemd service definitions"
+echo ">> POST-INSTALL: Reloading systemd service definitions"
 /usr/bin/systemctl daemon-reload
 
-su - blacklight -s /bin/bash
-cd "${HOME}"
+echo ">> POST-INSTALL: Stopping Solr and Blacklight"
+/usr/bin/systemctl stop solr.service
+/usr/bin/systemctl stop blacklght.service
+
+echo ">> POST-INSTALL: Login as Blacklight user and perform tasks"
+
+su - blacklight -s /bin/bash <<_EOF_
+
+set -o errexit
+
+cd \${HOME}
 
 source /usr/local/rvm/scripts/rvm
-/usr/local/rvm/bin/rvm use 2.3.1
+rvm use 2.3.1
 
 echo "Performing database migrations"
 RAILS_ENV=production /opt/project-blacklight/bin/rake db:migrate
@@ -22,3 +31,8 @@ echo "Precompiling assets"
 RAILS_ENV=production /opt/project-blacklight/bin/rake assets:precompile
 
 exit 0
+_EOF_
+
+echo ">> POST-INSTALL: Start Solr and Blacklight"
+/usr/bin/systemctl start solr.service
+/usr/bin/systemctl start blacklight.service
