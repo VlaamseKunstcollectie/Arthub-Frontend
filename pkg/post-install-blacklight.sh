9a1dd6fcf3148bf31e5dbe8452fbd594fcc85de0
jonas.de.moor <jonas.de.moor@inuits.eu>
2018-06-24 20:40:49 +0200

    pkg/post-install-blacklight.sh: do not use absolute path for rake

--- pkg/post-install-blacklight.sh
+++ pkg/post-install-blacklight.sh
@@ -25,10 +25,10 @@ source /usr/local/rvm/scripts/rvm
 rvm use 2.3.1
 
 echo "Performing database migrations"
-RAILS_ENV=production /opt/project-blacklight/bin/rake db:migrate
+RAILS_ENV=production rake db:migrate
 
 echo "Precompiling assets"
-RAILS_ENV=production /opt/project-blacklight/bin/rake assets:precompile
+RAILS_ENV=production rake assets:precompile
 
 exit 0
 _EOF_
