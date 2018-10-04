ac48419a216a592c8e3bc3d742f22f9976baf542
Matthias Vandermaesen <matthias@colada.be>
2018-10-04 17:10:03 +0200

    Added: Google Analytics code.

---
+++ app/assets/javascripts/google_analytics.js
@@ -0,0 +1,7 @@
+document.addEventListener('turbolinks:load', function(event) {
+  if (typeof ga === 'function') {
+    ga('set', 'location', event.data.url);
+    ga('send', 'pageview');
+    ga('set', 'anonymizeIp', true);
+  }
+});
